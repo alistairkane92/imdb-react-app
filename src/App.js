02c12ecb0d4d4af635b22f95e9d5dccdafe8cea8
Alistair Kane <vplelmo@hotmail.co.uk>
2018-01-08 18:30:50 +0000

    adds showopenings and links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import ShowOpenings from './components/ShowOpenings'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <ShowOpenings/>
     );
   }
 }
---
+++ src/components/ShowOpenings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ShowOpenings extends React.Component {
+  render(){
+    return(
+      <h1>test</h1>
+    )
+  }
+}
+
+export default ShowOpenings
